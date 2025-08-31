c203bb6c70063bc476386067269edbc4495c5b28
sharon <sharonba@gmail.com>
2025-08-31 21:01:38 +0300

    added points for users added leaderboard api added get user by id

--- trivia.py
+++ trivia.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from utils.utils import trivia
+from utils.utils import trivia, users
 
 current_id = 1
 trivia_bp = Blueprint('trivia_bp', __name__)
@@ -8,6 +8,7 @@ trivia_bp = Blueprint('trivia_bp', __name__)
 def show_trivia():
     return trivia
 
+#adding questions
 @trivia_bp.post('/add')
 def add_trivia():
     global  current_id
@@ -23,10 +24,25 @@ def add_trivia():
     except Exception as e:
         return str(e), 200
 
-
+#getting questions by id
 @trivia_bp.get('/question/<int:id>')
 def get_question(id):
     for question in trivia:
         if question.get('id') == id:
             return question
     return jsonify({'status': 'not found'}), 404
+
+#adding points to users
+@trivia_bp.put('/correct/<int:id>')
+def add_points(id):
+    for user in users:
+        if user["id"] == id:
+            user["points"] += 1
+            return f"point added to {user["username"]}"
+    return "No user found", 404
+
+@trivia_bp.get('/leaderboards')
+def show_leaderboard():
+    sorted_data = sorted(users, key=lambda x: x["points"], reverse=True)
+    return [{"username": u["username"], "points": u["points"]} for u in sorted_data]
+
--- users.py
+++ users.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, request
-
 from werkzeug.security import generate_password_hash, check_password_hash
-
-from utils.utils import users
+from utils.utils import users, points
 
 current_id = 1
+
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -13,11 +12,22 @@ user_bp = Blueprint('user_bp', __name__)
 def show_users():
     return users
 
+#get user by id
+@user_bp.get('/<int:id>')
+def show_by_id(id):
+    for user in users:
+        if user["id"] == id:
+            return user
+    return "No user found" , 404
+
+#add user
 @user_bp.post('/signup')
 def signup():
     global current_id
     try:
         data = request.json
+        data["id"] = current_id
+        data["points"] = points
         if 'password' not in request.json.keys() or 'username' not in request.json.keys():
             return 'name and password are required ', 403
         data['password'] = generate_password_hash(data['password'])
@@ -27,6 +37,7 @@ def signup():
                 return 'user already exists', 403
 
         users.append(data)
+        current_id += 1
         # user1 = Users(username=data['username'], password=data['password'])
         # db.session.add(user1)
         # db.session.commit()
@@ -35,17 +46,17 @@ def signup():
     except Exception as e:
         return str(e), 200
 
-
+#login as user
 @user_bp.post('/login')
 def login():
     username = request.json['username']
     password = request.json['password']
 
-    user_data = get_user(username)
+    user_data = get_users(username)
     if not user_data or not check_password_hash(user_data['password'], password):
         return "username or password incorrect", 401
-    return "Login successful"
+    return "Login successful", 200
 
 
-def get_user(username):
+def get_users(username):
     return [u for u in users if u['username'] == username][0]
